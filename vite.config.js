70dce07e6604e9748e2dc809a03ed388fbe1c6ea
song <keyoos@163.com>
2024-04-26 19:11:48 +0800

    判断mode模式,在build下无需提示环境配置

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,26 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 import { serverPrompts } from './prompts/index.js'
 
-const serverOptions = await serverPrompts()
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: serverOptions.host,  // 启动后浏览器窗口输入地址就可以进行访问
-    port: serverOptions.port, // 端口号
-  },
+export default defineConfig(async ({ mode }) => {
+  let serverOptions = {}
+  if (mode === 'development') {
+    serverOptions = await serverPrompts()
+  }
+  return {
+    plugins: [
+      vue(),
+      VueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: serverOptions.host,  // 启动后浏览器窗口输入地址就可以进行访问
+      port: serverOptions.port, // 端口号
+    },
+  }
 })
